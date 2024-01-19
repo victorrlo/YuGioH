86a76f737597428073403e54493a3c5b73b6519d
victorrlo <42052096+victorrlo@users.noreply.github.com>
2024-01-19 01:28:08 -0300

    Lógica das cartas em campo

--- scripts/engine.js
+++ scripts/engine.js
@@ -52,10 +52,37 @@ const cardData = [
 }
 ]
 
+async function getRandomCardId(){
+    const randomIndex = Math.floor(random() * cardData.length);
+    return cardData[randomIndex].id;
+}
+
+async function createCardImage(IdCard, fieldSide){
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src",  `${pathImages}card-back.png`);
+    cardImage.setAttribute("data-id",  IdCard);
+    cardImage.classList.add("card");
+
+    if(fieldSide === playerSides.player1){
+        cardImage.addEventListener("click", ()=>{
+            setCardsField(cardImage.getAttribute("data-id"));
+        });
+    }
+
+    cardImage.addEventListener("mouseover", ()=>{
+        drawSelectCard(IdCard);
+    });
+
+    return cardImage;
+}
+
 async function drawCards(cardNumbers, fieldSide){
     for(let i=0; i<cardNumbers; i++){
         const randomIdCard = await getRandomCardId();
         const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage);
     }
 }
 
